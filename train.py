1dc7dcf1657030c1477087aa3266fe26f9a80fb3
mengfeil <mengfei.li@intel.com>
2022-10-27 22:46:15 -0700

    enable oob script

--- train.py
+++ train.py
@@ -1,8 +1,10 @@
 # coding: utf-8
+from codecs import strict_errors
 import os
 import time
 import sys
 import yaml
+import argparse
 import numpy as np
 import pandas as pd
 from src.util import ExeDataset,write_pred
@@ -14,10 +16,24 @@ import torch.optim as optim
 from torch.autograd import Variable
 
 
+parser = argparse.ArgumentParser(description='PyTorch Inference.')
+parser.add_argument('--config_path', default='config/example.yaml', type=str, help='config file')
+parser.add_argument('--seed', default=123, type=int, help='random seed')
+parser.add_argument('--device', default="cpu", type=str, help='cpu, cuda or xpu')
+parser.add_argument('--batch_size', default=1, type=int, help='batch size')
+parser.add_argument('--precision', default="float32", type=str, help='precision')
+parser.add_argument('--channels_last', default=1, type=int, help='Use NHWC or not')
+parser.add_argument('--profile', action='store_true', default=False, help='collect timeline')
+parser.add_argument('--num_iter', default=1, type=int, help='test iterations')
+parser.add_argument('--num_warmup', default=0, type=int, help='test warmup')
+args = parser.parse_args()
+print(args)
+
+
 # Load config file for experiment
 try:
-    config_path = sys.argv[1]
-    seed = int(sys.argv[2])
+    config_path = args.config_path
+    seed = int(args.seed)
     conf = yaml.load(open(config_path,'r'))
 except:
     print('Usage: python3 run_exp.py <config file path> <seed>')
@@ -47,12 +63,14 @@ chkpt_acc_path = checkpoint_dir+exp_name+'.model'
 pred_path = pred_dir+exp_name+'.pred'
 
 # Parameters
-use_gpu = conf['use_gpu']
+# use_gpu = conf['use_gpu']
+use_gpu = True if args.device == "cuda" and torch.cuda.is_available() else False
 use_cpu = conf['use_cpu']
 learning_rate = conf['learning_rate']
 max_step = conf['max_step']
 test_step = conf['test_step']
-batch_size = conf['batch_size']
+# batch_size = conf['batch_size']
+batch_size = args.batch_size
 first_n_byte = conf['first_n_byte']
 window_size = conf['window_size']
 display_step = conf['display_step']
@@ -106,6 +124,10 @@ bce_loss = nn.BCEWithLogitsLoss()
 adam_optim = optim.Adam([{'params':malconv.parameters()}],lr=learning_rate)
 sigmoid = nn.Sigmoid()
 
+if args.channels_last:
+    malconv = malconv.to(memory_format=torch.channels_last)
+    print("---- Use NHWC model ")
+
 if use_gpu:
     malconv = malconv.cuda()
     bce_loss = bce_loss.cuda()
@@ -127,62 +149,103 @@ total_step = 0
 step_cost_time = 0
 
 
-while total_step < max_step:
-    
-    # Training 
-    for step,batch_data in enumerate(dataloader):
-        start = time.time()
+def train(dataloader, validloader, valid_best_acc, args):
+    while total_step < max_step:
         
-        adam_optim.zero_grad()
-        
-        cur_batch_size = batch_data[0].size(0)
+        # Training 
+        for step,batch_data in enumerate(dataloader):
+            start = time.time()
+            
+            adam_optim.zero_grad()
+            
+            cur_batch_size = batch_data[0].size(0)
 
-        exe_input = batch_data[0].cuda() if use_gpu else batch_data[0]
-        exe_input = Variable(exe_input.long(),requires_grad=False)
-        
-        label = batch_data[1].cuda() if use_gpu else batch_data[1]
-        label = Variable(label.float(),requires_grad=False)
-        
-        pred = malconv(exe_input)
-        loss = bce_loss(pred,label)
-        loss.backward()
-        adam_optim.step()
-        
-        history['tr_loss'].append(loss.cpu().data.numpy()[0])
-        history['tr_acc'].extend(list(label.cpu().data.numpy().astype(int)==(sigmoid(pred).cpu().data.numpy()+0.5).astype(int)))
-        
-        step_cost_time = time.time()-start
-        
-        if (step+1)%display_step == 0:
-            print(step_msg.format(total_step,np.mean(history['tr_loss']),
-                                  np.mean(history['tr_acc']),step_cost_time),end='\r',flush=True)
-        total_step += 1
+            exe_input = batch_data[0].cuda() if use_gpu else batch_data[0]
+            exe_input = Variable(exe_input.long(),requires_grad=False)
+            
+            label = batch_data[1].cuda() if use_gpu else batch_data[1]
+            label = Variable(label.float(),requires_grad=False)
+            
+            pred = malconv(exe_input)
+            loss = bce_loss(pred,label)
+            loss.backward()
+            adam_optim.step()
+            
+            history['tr_loss'].append(loss.cpu().data.numpy())
+            history['tr_acc'].extend(list(label.cpu().data.numpy().astype(int)==(sigmoid(pred).cpu().data.numpy()+0.5).astype(int)))
+            
+            step_cost_time = time.time()-start
+            
+            if (step+1)%display_step == 0:
+                print(step_msg.format(total_step,np.mean(history['tr_loss']),
+                                    np.mean(history['tr_acc']),step_cost_time),end='\r',flush=True)
+            total_step += 1
 
-        # Interupt for validation
-        if total_step%test_step ==0:
-            break
-    
-    
+            # Interupt for validation
+            if total_step%test_step ==0:
+                break
+            
+            evaluate(validloader, valid_best_acc, args)
+
+
+def evaluate(validloader, valid_best_acc, args):
     # Testing
     history['val_loss'] = []
     history['val_acc'] = []
     history['val_pred'] = []
-    
+
+    total_time = 0.0
+    total_sample = 0
     for _,val_batch_data in enumerate(validloader):
         cur_batch_size = val_batch_data[0].size(0)
 
         exe_input = val_batch_data[0].cuda() if use_gpu else val_batch_data[0]
+        if args.channels_last:
+            try:
+                exe_input = exe_input.contiguous(memory_format=torch.channels_last)
+            except Exception as e:
+                pass
         exe_input = Variable(exe_input.long(),requires_grad=False)
 
         label = val_batch_data[1].cuda() if use_gpu else val_batch_data[1]
-        label = Variable(label.float(),requires_grad=False)
+        label = Variable(label.float(),requires_grad=True)
 
-        pred = malconv(exe_input)
-        loss = bce_loss(pred,label)
+        if args.profile:
+            with torch.profiler.profile(
+                activities=[torch.profiler.ProfilerActivity.CPU],
+                record_shapes=True,
+                schedule=torch.profiler.schedule(
+                    wait=int(args.num_iter/2),
+                    warmup=2,
+                    active=1,
+                ),
+                on_trace_ready=trace_handler,
+            ) as p:
+                for i in range(args.num_iter):
+                    elapsed = time.time()
+                    pred = malconv(exe_input)
+                    loss = bce_loss(pred,label)
+                    elapsed = time.time() - elapsed
+                    p.step()
+                    print("Iteration: {}, inference time: {} sec.".format(i, elapsed), flush=True)
+                    if i >= args.num_warmup:
+                        total_time += elapsed
+                        total_sample += args.batch_size
+        else:
+            for i in range(args.num_iter):
+                elapsed = time.time()
+                pred = malconv(exe_input)
+                loss = bce_loss(pred,label)
+                elapsed = time.time() - elapsed
+                print("Iteration: {}, inference time: {} sec.".format(i, elapsed), flush=True)
+                if i >= args.num_warmup:
+                    total_time += elapsed
+                    total_sample += args.batch_size
 
-        history['val_loss'].append(loss.cpu().data.numpy()[0])
-        history['val_acc'].extend(list(label.cpu().data.numpy().astype(int)==(sigmoid(pred).cpu().data.numpy()+0.5).astype(int)))
-        history['val_pred'].append(list(sigmoid(pred).cpu().data.numpy()))
+        history['val_loss'].append(loss.cpu().float().data.numpy())
+        history['val_acc'].extend(list(label.cpu().float().data.numpy().astype(int)==(sigmoid(pred).cpu().float().data.numpy()+0.5).astype(int)))
+        history['val_pred'].append(list(sigmoid(pred).cpu().float().data.numpy()))
+        break
 
     print(log_msg.format(total_step, np.mean(history['tr_loss']), np.mean(history['tr_acc']),
                     np.mean(history['val_loss']), np.mean(history['val_acc']),step_cost_time),
@@ -190,6 +253,9 @@ while total_step < max_step:
     
     print(valid_msg.format(total_step,np.mean(history['tr_loss']),np.mean(history['tr_acc']),
                            np.mean(history['val_loss']),np.mean(history['val_acc'])))
+    throughput = total_sample / total_time
+    print("inference Throughput:\t {:.2f} samples/s".format(throughput))
+    return
     if valid_best_acc < np.mean(history['val_acc']):
         valid_best_acc = np.mean(history['val_acc'])
         torch.save(malconv,chkpt_acc_path)
@@ -200,3 +266,27 @@ while total_step < max_step:
     history['tr_loss'] = []
     history['tr_acc'] = []
 
+def trace_handler(p):
+    output = p.key_averages().table(sort_by="self_cpu_time_total")
+    print(output)
+    import pathlib
+    timeline_dir = str(pathlib.Path.cwd()) + '/timeline/'
+    if not os.path.exists(timeline_dir):
+        try:
+            os.makedirs(timeline_dir)
+        except:
+            pass
+    timeline_file = timeline_dir + 'timeline-' + str(torch.backends.quantized.engine) + '-' + \
+                'MalConv-' + str(p.step_num) + '-' + str(os.getpid()) + '.json'
+    p.export_chrome_trace(timeline_file)
+
+
+if __name__ == "__main__":
+    if args.precision == "bfloat16":
+        with torch.cpu.amp.autocast(enabled=True, dtype=torch.bfloat16):
+            evaluate(validloader, valid_best_acc, args)
+    elif args.precision == "float16":
+        with torch.cuda.amp.autocast(enabled=True, dtype=torch.float16):
+            evaluate(validloader, valid_best_acc, args)
+    else:
+        evaluate(validloader, valid_best_acc, args)
