967d1f493d789ae79cb10648e2f350aa290b874c
jun.du@intel.com <jun.du@intel.com>
2023-12-12 23:40:44 -0800

    update cuda run with float16

--- train.py
+++ train.py
@@ -290,11 +290,11 @@ def trace_handler(p):
 if __name__ == "__main__":
     if args.precision == "bfloat16":
         print("---- Use cpu AMP bfloat16")
-        with torch.cpu.amp.autocast(enabled=True, dtype=torch.bfloat16):
+        with torch.autocast(device_type="cuda" if torch.cuda.is_available() else "cpu", enabled=True, dtype=torch.bfloat16):
             evaluate(validloader, valid_best_acc, args)
     elif args.precision == "float16":
         print("---- Use cuda AMP float16")
-        with torch.cpu.amp.autocast(enabled=True, dtype=torch.half):
+        with torch.autocast(device_type="cuda" if torch.cuda.is_available() else "cpu", enabled=True, dtype=torch.half):
             evaluate(validloader, valid_best_acc, args)
     else:
         evaluate(validloader, valid_best_acc, args)
